71c204c515845fb4f894d99ab0b903ceffc6bccb
chaitanyagarg2007-hue <chaitanyagarg2007@gmail.com>
2025-11-16 11:01:57 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,21 +1,24 @@
-import mysql from "mysql2/promise"; 
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
-const db = mysql.createPool({ 
-  host: "localhost", 
-  user: "root",
-  password: "1234", 
-  database: "flight_booking_db",
+dotenv.config();
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
 db.getConnection()
-  .then(connection => {
+  .then((connection) => {
     console.log("Database connection successful!");
     connection.release();
   })
-  .catch(err => {
+  .catch((err) => {
     console.error("Database connection failed:", err.message);
     process.exit(1);
   });
